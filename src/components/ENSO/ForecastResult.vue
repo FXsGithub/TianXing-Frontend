70bd69dd57a796a24547f1ffb60356a20443a563
青袍君 <1455670697@qq.com>
2023-07-20 19:23:20 +0800

    增加了tabs

--- src/components/ENSO/ForecastResult.vue
+++ src/components/ENSO/ForecastResult.vue
@@ -7,6 +7,23 @@
     <h1>
       ENSO预报结果
     </h1>
+    <el-tabs type="border-card">
+      <el-tab-pane label="内容1">
+        <p>内容1</p>
+        <p>测试内容</p>
+      </el-tab-pane>
+      <el-tab-pane label="内容2">
+        <p>内容2</p>
+      </el-tab-pane>
+      <el-tab-pane label="内容3">
+        <p>内容3</p>
+      </el-tab-pane>
+      <el-tab-pane label="内容4">
+        <p>内容4</p>
+      </el-tab-pane>
+    </el-tabs>
+    <div id="echarts-demo">
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+// import * as echarts from 'echarts';
+
 
 const app = createApp(App)
 
