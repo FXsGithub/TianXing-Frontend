ef3436d286c9e1be8c732031cf85a70323edf011
青袍君 <1455670697@qq.com>
2023-07-21 03:50:45 +0800

    增加了一个使用echarts的示例代码

--- src/components/ENSO/ForecastResult.vue
+++ src/components/ENSO/ForecastResult.vue
@@ -1,5 +1,36 @@
 <script setup>
 
+
+import {onMounted} from "vue";
+import * as echarts from 'echarts';
+import {nextTick} from "vue";
+
+onMounted(
+    ()=>{
+      nextTick(() => {
+        let myChart = echarts.init(document.getElementById('chart1'));
+        myChart.setOption({
+          title: {
+            text: 'ECharts 入门示例'
+          },
+          tooltip: {},
+          xAxis: {
+            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+          },
+          yAxis: {},
+          series: [
+            {
+              name: '销量',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+            }
+          ]
+        });
+
+      })
+    }
+)
+
 </script>
 
 <template>
@@ -11,6 +42,7 @@
       <el-tab-pane label="内容1">
         <p>内容1</p>
         <p>测试内容</p>
+        <div class="chart" id="chart1"></div>
       </el-tab-pane>
       <el-tab-pane label="内容2">
         <p>内容2</p>
@@ -28,5 +60,7 @@
 </template>
 
 <style scoped lang="scss">
-
+  .chart{
+    height: 200px;
+  }
 </style>
