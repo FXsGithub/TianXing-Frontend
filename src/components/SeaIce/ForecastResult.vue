e936cd19b464e1e5da1fef29120efbb329d6cf87
wyfyjc <1819351418@qq.com>
2023-07-25 22:36:41 +0800

    添加了使用axios的部分和横轴的动态更改

--- src/components/SeaIce/ForecastResult.vue
+++ src/components/SeaIce/ForecastResult.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import {onMounted} from "vue";
+import { reactive } from "vue";
 import {ref} from "vue";
 import * as echarts from 'echarts';
 import {nextTick} from "vue";
@@ -9,10 +10,6 @@ import { configProviderContextKey } from "element-plus";
 const currentDate = new Date();
 const year = currentDate.getFullYear() - 1 + '';
 const month = currentDate.getMonth() < 10 ? '0' + (currentDate.getMonth() + 1 + '') : currentDate.getMonth() + 1 + ''
-
-
-const SIEChartDescription = '此处为SIE指数描述内容'
-const SICChartDescription = '此处为SIC模态描述内容'
 
 const selectedYear = ref('');
 const selectedMonth = ref('');
@@ -25,15 +22,32 @@ let myChart;
 const chartTitle = ref('')
 chartTitle.value = `${selectedYear.value}年${selectedMonth.value}月~${Number(selectedYear.value) + 1 + ''}年${selectedMonth.value}月 SIE指数预测结果`
 
-function updateChartTitle() {
-  chartTitle.value = `${selectedYear.value}年${selectedMonth.value}月~${Number(selectedYear.value) + 1 + ''}年${selectedMonth.value}月 SIE指数预测结果`;
-  myChart.setOption({
-    title: {
-      text: chartTitle.value,
-      left: 'center' //标题水平居中
-    },
-  });
+const months = {
+  '01': '一月',
+  '02': '二月',
+  '03': '三月',
+  '04': '四月',
+  '05': '五月',
+  '06': '六月',
+  '07': '七月',
+  '08': '八月',
+  '09': '九月',
+  '10': '十月',
+  '11': '十一月',
+  '12': '十二月'
 }
+
+const SIEData = reactive({
+  prediction: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  mean: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  upper: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  lower: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  description : ''
+});
+
+const SICData = reactive({
+  SICChartDescription : '此处为SIC模态描述内容'
+});
 
 onMounted(
     ()=>{
@@ -63,22 +77,22 @@ onMounted(
             {
               name: 'prediction',
               type: 'line',
-              data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+              data: SIEData.prediction
             },
             {
               name: 'mean',
               type: 'line',
-              data: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+              data: SIEData.mean
             },
             {
               name: 'upper',
               type: 'line',
-              data: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+              data: SIEData.upper
             },
             {
               name: 'lower',
               type: 'line',
-              data: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+              data: SIEData.lower
             },
           ]
         });
@@ -87,6 +101,105 @@ onMounted(
     }
 )
 
+// setTimeout(() => {
+//   SIEData.prediction = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//   SIEData.mean = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+//   SIEData.upper = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+//   SIEData.lower = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+
+//   myChart.setOption({
+//     series: [
+//       {
+//         name: 'prediction',
+//         type: 'line',
+//         data: SIEData.prediction
+//       },
+//       {
+//         name: 'mean',
+//         type: 'line',
+//         data: SIEData.mean
+//       },
+//       {
+//         name: 'upper',
+//         type: 'line',
+//         data: SIEData.upper
+//       },
+//       {
+//         name: 'lower',
+//         type: 'line',
+//         data: SIEData.lower
+//       },
+//     ]
+//   });
+// }, 2000);
+
+// 请求SIE数据
+const updateChart = async () => {
+  chartTitle.value = `${selectedYear.value}年${selectedMonth.value}月~${Number(selectedYear.value) + 1 + ''}年${selectedMonth.value}月 SIE指数预测结果`;
+
+  try {
+    const requestData = {
+      year: selectedYear.value,
+      month: selectedMonth.value
+    };
+
+    // const response = await axios.post('api/SeaIce/SIE', requestData);
+
+    // SIEData.prediction = response.data.prediction;
+    // SIEData.mean = response.data.mean;
+    // SIEData.upper = response.data.upper;
+    // SIEData.lower = response.data.lower;
+    // SIEData.description = response.data.description;
+
+    // 测试数据
+    SIEData.prediction = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    SIEData.mean = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+    SIEData.upper = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+    SIEData.lower = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+    SIEData.description = '此处为SIE指数描述内容';
+
+    myChart.setOption({
+      xAxis: {
+        data: monthArray()
+      },
+      title: {
+        text: chartTitle.value,
+        left: 'center' //标题水平居中
+      },
+      series: [
+        {
+          name: 'prediction',
+          type: 'line',
+          data: SIEData.prediction
+        },
+        {
+          name: 'mean',
+          type: 'line',
+          data: SIEData.mean
+        },
+        {
+          name: 'upper',
+          type: 'line',
+          data: SIEData.upper
+        },
+        {
+          name: 'lower',
+          type: 'line',
+          data: SIEData.lower
+        },
+      ]
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function monthArray() {
+  const index = Object.keys(months).indexOf(selectedMonth.value);
+  const keys = Object.keys(months);
+  return keys.slice(index).map(key => months[key]).concat(keys.slice(0, index).map(key => months[key]));
+}
 </script>
 
 <template>
@@ -95,21 +208,21 @@ onMounted(
       {{ selectedYear }}年{{ selectedMonth }}月~{{ Number(selectedYear) + 1 + '' }}年{{ selectedMonth }}月 海冰预测结果
     </h1>
     <div class="datePickerContainer">
-      <el-date-picker @change="updateChartTitle" v-model="selectedYear" type="year" format="YYYY" value-format="YYYY" :clearable="false" style="width: 80px; height: 25px"/>
+      <el-date-picker @change="updateChart" v-model="selectedYear" type="year" format="YYYY" value-format="YYYY" :clearable="false" style="width: 80px; height: 25px"/>
       <div class="text">年</div>
-      <el-date-picker @change="updateChartTitle" v-model="selectedMonth" type="month" format="MM" value-format="MM" :clearable="false" style="width: 60px; height: 25px"/>
+      <el-date-picker @change="updateChart" v-model="selectedMonth" type="month" format="MM" value-format="MM" :clearable="false" style="width: 60px; height: 25px"/>
       <div class="text">月</div>
     </div>    
-    <el-tabs type="border-card">
+    <el-tabs type="border-card" stretch="true">
       <el-tab-pane label="SIE指数">
         <div class="chart" id="SICChart"></div>
         <div class="description">
-          {{ SIEChartDescription }}
+          {{ SIEData.description }}
         </div>
       </el-tab-pane>
       <el-tab-pane label="SIC模态">
         <div class="description">
-          {{ SICChartDescription }}
+          {{ SICData.description }}
         </div>
       </el-tab-pane>
     </el-tabs>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import './style.scss'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from './App.vue'
+import axios from "axios";
 // import * as echarts from 'echarts';
-
+//axios.defaults.baseURL = 'http://127.0.0.1:1234'
 
 const app = createApp(App)
 
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
