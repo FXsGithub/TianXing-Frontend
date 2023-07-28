72e0af2ae26bcf82a5243b839a9b37fbf6f57b51
青袍君 <1455670697@qq.com>
2023-07-28 21:31:36 +0800

    添加了新闻列表动态更改

--- src/components/About/News.vue
+++ src/components/About/News.vue
@@ -1,11 +1,31 @@
 <script setup>
 
+const files = import.meta.glob('../../news/*.md')
+let newsFiles = Object.keys(files).map(filePath => {
+  // 取出文件名
+  return filePath.split('/').pop().replace('.md', '')
+})
+
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const isNewsIndex = computed(() => route.path === '/news' || route.path === '/News' )
+
 </script>
 
 <template>
   <div class="pageContent">
 
-    <h1>信息发布页</h1>
+    <div v-if="isNewsIndex">
+      <h1 >信息发布</h1>
+
+      <li v-for="file in newsFiles" :key="file">
+        <router-link :to="`/news/${file}`">{{ file }}</router-link>
+      </li>
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
