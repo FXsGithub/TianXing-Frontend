b916131668b49fe502845c71ae0be921cb79579c
otthong <2022652043@qq.com>
2023-08-09 02:47:20 +0800

    ENSO预测结果——指数预测  接入真实接口+实现时间限制

--- src/components/ENSO/ForecastResult.vue
+++ src/components/ENSO/ForecastResult.vue
@@ -1,4 +1,7 @@
 <script setup>
+  //start_year       selectedDateTime
+//currentDate         date
+
   import {ref, onMounted, reactive, watch} from "vue";
 import * as echarts from "echarts";
 import axios  from "axios";
@@ -9,7 +12,11 @@ import {
   ArrowLeft,
   ArrowRight,
 } from '@element-plus/icons-vue'
+
+
+const prefix="https://www.tjensoprediction.com"
 /* 时间选择器 -- begin */
+
 const currentDate = new Date('2023-2');   //  赋初值
 const year = currentDate.getFullYear() + '';
 const month = currentDate.getMonth() < 10 ? '0' + (currentDate.getMonth() + 1 + '') : currentDate.getMonth() + 1 + ''
@@ -18,7 +25,12 @@ const start_month = ref(month);   //选择的月
 
 const start_time = ref(null);     //可选时间范围
 const end_time = ref(null);
-/*
+
+
+
+
+
+/*预留的
 此处调接口获取时间范围
 axios.get('http://www.tjensoprediction.com:8080/imgs/WEA_T2M/getInitData')
 .then(res =>{
@@ -26,7 +38,8 @@ axios.get('http://www.tjensoprediction.com:8080/imgs/WEA_T2M/getInitData')
   end_time.value = new Date(res.data.latestDate);
 });
 */
-start_time.value = new Date('2023-1');      //暂时写死范围
+
+start_time.value = new Date('2023-1');      //后端还没完成嘿嘿暂时写死范围
 end_time.value = new Date('2023-6');
 const limitedDateRange = (time) => {
   return time.getTime() < start_time.value || time.getTime() > end_time.value;
@@ -36,61 +49,120 @@ const limitedDateRange = (time) => {
 /* 根据选择页更新限制范围 */
 function handleClick(tab, event) {
   console.log(tab.props.label);
-  if(tab.props.label == '指数预测'){
-    start_time.value = new Date('2023-1');      //暂时写死范围
-    end_time.value = new Date('2023-7');
-  }
-  else if(tab.props.label == '模态预测'){ 
+ if(tab.props.label == '模态预测'){ 
     start_time.value = new Date('2023-2');      //暂时写死范围
     end_time.value = new Date('2023-4');
+    
+    axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year='+Number(start_year.value)+'&month='+Number(start_month.value))
+      .then(res => {
+      index_heat = 0;//组图指针置0
+      console.log(res.data);
+      imgSrc_of_heat_Array = res.data;
+      imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;
+      
+      
+    });
   }
- 
+  else if(tab.props.label == '指数预测'){
+    start_time.value = new Date('2023-1');      //暂时写死范围
+    end_time.value = new Date('2023-6');
+
+  axios.get('http://www.tjensoprediction.com:8080/enso/predictionResult/linechart?year='+Number(start_year.value)+'&month='+Number(start_month.value))
+    .then(res => {
+      
+      chart1.value = res.data
+     
+    })
+  }
 }
 /* 时间选择器 -- end */
   
   
-var index_month=0; //切换页时修改这个索引
+
 
 const chart1 = ref({})
 const chart1Title = ref('**年*月~**年*月Niño3.4指数结果预测')
-  
-  
-  /* chart1  的下方文字描述 */
   let Chart1_Description = reactive({single:true, text:'此处为预测结果指数预测折线图。'})
 
  
- // 柱状图
-var dom = document.getElementById("container");
+  var index_heat=0; //切换热力图时修改这个索引
+  var imgSrc_of_heat_Array ;
+  const imgSrc_of_heat = ref({})
 
-// 初始化图表
 
 
-  
+
   /* 赋初值 */
   //指数预测
 
 axios.get('http://www.tjensoprediction.com:8080/enso/predictionResult/linechart?year='+Number(start_year.value)+'&month='+Number(start_month.value))
     .then(res => {
       chart1.value = res.data
-      console.log(chart1.value)
-      Chart1_Description.text = res.data.text
+     
+    });
+
+//模态预测（热力图）
+axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year='+Number(start_year.value)+'&month='+Number(start_month.value))
+      .then(res => {
+      index_heat = 0;//组图指针置0
+      console.log(res.data);
+      imgSrc_of_heat_Array = res.data;
+      imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;
+      
     });
 
 
 
-
-
   /* 图表更新 */
-/* 图表更新 */
 function update_charts() {
-  axios.get('http://www.tjensoprediction.com:8080/enso/predictionExamination/monthlyComparison?year='+Number(start_year.value)+'&month='+Number(start_month.value))
+  
+ 
+      axios.get('http://www.tjensoprediction.com:8080/enso/predictionResult/linechart?year='+Number(start_year.value)+'&month='+Number(start_month.value))
+    .then(res => {
+      chart1.value = res.data
+     
+    })
+      axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year='+Number(start_year.value)+'&month='+Number(start_month.value))
+  
       .then(res => {
-        chart1.value = res.data.option
-        Chart1_Description.text = res.data.text
-      })
+        index_heat = 0;
+        console.log(res.data);
+        imgSrc_of_heat_Array = res.data;
+        imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;
+        
+
+      });
+
+
+
+
     }
-  
-  
+
+
+  /* 左右切换 -- begin */
+    function change_time_heat(flag) {
+
+if(flag==="left"){
+  if(index_heat>0){
+    index_heat--;
+  }
+  else{
+    index_heat=17;
+  }
+}
+else if(flag==="right"){
+  if(index_heat<17){
+    index_heat++;
+  }
+  else{
+    index_heat=0;
+  }
+}
+
+imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[index_heat]}`;
+
+
+}
   
     </script>
     
@@ -125,15 +197,38 @@ function update_charts() {
         <el-tabs type="border-card" @tab-click="handleClick">
 
           <el-tab-pane label="指数预测">
-            test
+            
             <v-chart class="chart_1" :option="chart1" autoresize > </v-chart>
             <p class="text1">{{Chart1_Description.text}}
             </p>
           </el-tab-pane>
 
+        
           <el-tab-pane label="模态预测">
-          </el-tab-pane>
-         
+        
+            <div class="pic_container">
+              <img class="picture" :src="imgSrc_of_heat" alt="">
+            </div>
+            <el-button type="primary" class="arrow-left" :icon="ArrowLeft" @click="change_time_heat('left')"></el-button>
+            <el-button type="primary" class="arrow-right" :icon="ArrowRight" @click="change_time_heat('right')"></el-button>
+          
+        </el-tab-pane>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </el-tabs>
       </div>
     </template>
@@ -161,6 +256,16 @@ function update_charts() {
       .text1{
         text-align: center;
       }
+      .picture {
+  width: 400px;
+  display: block; /* 将元素设置为块级元素 */
+  margin-left: 10px;
+  margin-top: -175px;
+  margin-bottom: -160px;
+}
+.pic_container{
+  overflow: hidden;
+}
       
       /* 预报误差页面的容器 */
       .chart-container {
