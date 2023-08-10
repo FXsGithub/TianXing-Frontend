6e4ccea3fdecde55e727fb2d30c519e9391fb1d1
otthong <2022652043@qq.com>
2023-08-11 00:32:33 +0800

    ENSO预测结果终于全部完成了！！

--- src/components/ENSO/ForecastResult.vue
+++ src/components/ENSO/ForecastResult.vue
@@ -1,18 +1,62 @@
 <script setup>
-  //start_year       selectedDateTime
-//currentDate         date
-
-import { ref, onMounted, reactive, watch, defineExpose } from "vue";
+  import {ref, onMounted, reactive, watch,defineExpose} from "vue";
 import * as echarts from "echarts";
 import axios  from "axios";
 import VChart from 'vue-echarts';
-import { nextTick } from "vue";
+import {nextTick} from "vue";
 import { configProviderContextKey } from "element-plus";
-import { ArrowLeft, ArrowRight } from '@element-plus/icons-vue'
+import {
+  ArrowLeft,
+  ArrowRight,
+} from '@element-plus/icons-vue'
 
 
 const prefix="https://www.tjensoprediction.com"
-/* 时间选择器 -- begin */
+
+//时间选择器范围框定--start
+const start_time = ref(null);
+const end_time = ref(null);
+/* 赋初值————默认为指数预测 */
+axios.get('http://www.tjensoprediction.com:8080/enso/linechart/getInitData')
+.then(res =>{
+  start_time.value = new Date(res.data.earliestDate);
+  end_time.value = new Date(res.data.latestDate);
+});
+
+const limitedDateRange = (time) => {
+  return time.getTime() < start_time.value || time.getTime() > end_time.value;
+};
+
+/* 根据选择页更新限制范围 */
+function handleClick(tab, event) {
+  console.log(tab.props.label);
+ if(tab.props.label == '模态预测'){ 
+
+//   axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta/getInitData')
+// .then(res =>{
+//   start_time.value = new Date(res.data.earliestDate);
+//   end_time.value = new Date(res.data.latestDate);
+// });
+
+    start_time.value='2023-2';
+    end_time.value='2023-4'
+  }
+  else if(tab.props.label == '指数预测'){
+
+    axios.get('http://www.tjensoprediction.com:8080/enso/linechart/getInitData')
+.then(res =>{
+  start_time.value = new Date(res.data.earliestDate);
+  end_time.value = new Date(res.data.latestDate);
+});
+
+
+  }
+}
+//时间选择器范围框定--end
+  
+
+
+
 
 const currentDate = new Date('2023-2');   //  赋初值
 const year = currentDate.getFullYear() + '';
@@ -20,72 +64,32 @@ const month = currentDate.getMonth() < 10 ? '0' + (currentDate.getMonth() + 1 + 
 const start_year = ref(year);     //选择的年
 const start_month = ref(month);   //选择的月
 
-const start_time = ref(null);     //可选时间范围
-const end_time = ref(null);
 
 
 
 
 
-/*预留的
-此处调接口获取时间范围
-axios.get('http://www.tjensoprediction.com:8080/imgs/WEA_T2M/getInitData')
+
+// 此处调接口获取时间范围
+axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta/getInitData')
 .then(res =>{
   start_time.value = new Date(res.data.earliestDate);
   end_time.value = new Date(res.data.latestDate);
 });
-*/
 
-start_time.value = new Date('2023-1');      //后端还没完成嘿嘿暂时写死范围
-end_time.value = new Date('2023-6');
-const limitedDateRange = (time) => {
-  return time.getTime() < start_time.value || time.getTime() > end_time.value;
-};
-
-
-/* 根据选择页更新限制范围 */
-function handleClick(tab, event) {
-  console.log(tab.props.label);
- if(tab.props.label == '模态预测'){ 
-    start_time.value = new Date('2023-2');      //暂时写死范围
-    end_time.value = new Date('2023-4');
-    
-    axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year='+Number(start_year.value)+'&month='+Number(start_month.value))
-      .then(res => {
-      index_heat = 0;//组图指针置0
-      console.log(res.data);
-      imgSrc_of_heat_Array = res.data;
-      imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;
-      
-      
-    });
-  }
-  else if(tab.props.label == '指数预测'){
-    start_time.value = new Date('2023-1');      //暂时写死范围
-    end_time.value = new Date('2023-6');
-
-  axios.get('http://www.tjensoprediction.com:8080/enso/predictionResult/linechart?year='+Number(start_year.value)+'&month='+Number(start_month.value))
-    .then(res => {
-      
-      chart1.value = res.data
-     
-    })
-  }
-}
-/* 时间选择器 -- end */
-  
-  
 
 
 const chart1 = ref({})
 const chart1Title = ref('**年*月~**年*月Niño3.4指数结果预测')
   let Chart1_Description = reactive({single:true, text:'此处为预测结果指数预测折线图。'})
 
- 
+  
   var index_heat=0; //切换热力图时修改这个索引
   var imgSrc_of_heat_Array ;
-  const imgSrc_of_heat = ref({})
+  var  title_of_heat_Array ;
 
+  const imgSrc_of_heat = ref({})
+  const title_of_heat = ref({})
 
 
 
@@ -101,10 +105,14 @@ axios.get('http://www.tjensoprediction.com:8080/enso/predictionResult/linechart?
 //模态预测（热力图）
 axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year='+Number(start_year.value)+'&month='+Number(start_month.value))
       .then(res => {
-      index_heat = 0;//组图指针置0
-      console.log(res.data);
-      imgSrc_of_heat_Array = res.data;
-      imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;
+      index_heat = 0;//图片数组指针置0
+      console.log(res.data.data);
+      imgSrc_of_heat_Array = res.data.data;  //res.data.data传递了一个图片数组
+     imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;    //const prefix="https://www.tjensoprediction.com"
+      
+
+     title_of_heat_Array = res.data.titles;
+      title_of_heat.value = title_of_heat_Array[0];
       
     });
 
@@ -112,63 +120,70 @@ axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year=
 
   /* 图表更新 */
 function update_charts() {
-  
- 
+   // 当日期时间选择发生变化时被调用
+
+  console.log(start_month.value); // 输出当前选择的日期和时间
+  console.log(start_year.value);
+
+  start_month.value=start_month.value;
+  start_month.value=start_month.value;
+
+  limitedDateRange();
       axios.get('http://www.tjensoprediction.com:8080/enso/predictionResult/linechart?year='+Number(start_year.value)+'&month='+Number(start_month.value))
     .then(res => {
       chart1.value = res.data
      
     })
-      axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year='+Number(start_year.value)+'&month='+Number(start_month.value))
-  
+    axios.get('http://www.tjensoprediction.com:8080/imgs/predictionResult/ssta?year='+Number(start_year.value)+'&month='+Number(start_month.value))
       .then(res => {
-        index_heat = 0;
-        console.log(res.data);
-        imgSrc_of_heat_Array = res.data;
-        imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;
-        
+      index_heat = 0;//图片数组指针置0
+      console.log(res.data.data);
+      imgSrc_of_heat_Array = res.data.data;  //res.data.data传递了一个图片数组
+     imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[0]}`;    //const prefix="https://www.tjensoprediction.com"
+      
 
-      });
+     title_of_heat_Array = res.data.titles;
+      title_of_heat.value = title_of_heat_Array[0];
+      
+    });
 
 
 
 
     }
 
-/* 使el-button点击后能正常失焦 Start (by wyf)*/
-const buttonLeft = ref(null);
-const buttonRight = ref(null);
 
   /* 左右切换 -- begin */
-function change_time_heat(flag) {
+    function change_time_heat(flag) {
 
-  if (flag === "left") {
-    if (index_heat > 0) {
-      index_heat--;
-    }
-    else {
-      index_heat = 17;
-    }
-    buttonLeft.value.$el.blur();
+if(flag==="left"){
+  if(index_heat>0){
+    index_heat--;
   }
-  else if (flag === "right") {
-    if (index_heat < 17) {
-      index_heat++;
-    }
-    else {
-      index_heat = 0;
-    }
-    buttonRight.value.$el.blur();
+  else{
+    index_heat=17;
   }
-  imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[index_heat]}`;
+}
+else if(flag==="right"){
+  if(index_heat<17){
+    index_heat++;
+  }
+  else{
+    index_heat=0;
+  }
 }
 
+imgSrc_of_heat.value = `${prefix}${imgSrc_of_heat_Array[index_heat]}`;
+title_of_heat.value = `${title_of_heat_Array[index_heat]}`;
+
+}
+  
+
 defineExpose({
-  change_time_heat
+  change_time_heat,
 });
 /* 使el-button点击后能正常失焦 End */
-
-</script>
+    </script>
     
     <template>
       <div class="pageContent">
@@ -210,12 +225,14 @@ defineExpose({
         
           <el-tab-pane label="模态预测">
         
+            <p class="picture_title">
+              {{ title_of_heat }}
+            </p>
             <div class="pic_container">
-              <img class="picture" :src="imgSrc_of_heat" alt="">
-            </div>
-            <el-button ref="buttonLeft" type="primary" class="arrow-left" :icon="ArrowLeft" @click="change_time_heat('left')"></el-button>
-            <el-button ref="buttonRight" type="primary" class="arrow-right" :icon="ArrowRight" @click="change_time_heat('right')"></el-button>
-          
+              <img class="picture" :src="imgSrc_of_heat" alt="" >
+            <el-button type="primary" class="arrow-left" :icon="ArrowLeft" @click="change_time_heat('left')"></el-button>
+            <el-button type="primary" class="arrow-right" :icon="ArrowRight" @click="change_time_heat('right')"></el-button>
+          </div>
         </el-tab-pane>
 
 
@@ -249,16 +266,19 @@ defineExpose({
         text-align: center;
       }
       .picture {
-  width: 400px;
+  width: 700px;
   display: block; /* 将元素设置为块级元素 */
-  margin-left: 10px;
-  margin-top: -175px;
-  margin-bottom: -160px;
+  margin: auto;
 }
+
 .pic_container{
   overflow: hidden;
 }
-      
+
+.picture_title {
+   text-align: center;
+   font-size: 18px;
+}
       /* 预报误差页面的容器 */
       .chart-container {
         position: relative;
